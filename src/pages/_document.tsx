464e6cd69666e323635d8d0e169e7ca20eaab760
ikilikid <ponponta1117@gmail.com>
2026-01-06 11:51:38 +0900

    adsense

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,16 +1,20 @@
 import { Head, Html, Main, NextScript } from "next/document";
-import Script from "next/script";
 
 export default function Document() {
   return (
-    <Html lang="jp">
+    <Html lang="ja">
       <Head>
-        {/* Google Maps API - Moved from _app.js */}
-        <Script
-          id="google-maps"
-          strategy="beforeInteractive"
+        {/* Google AdSense */}
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1440692612851416"
+          crossOrigin="anonymous"
+        ></script>
+
+        {/* Google Maps API */}
+        <script
           src={`https://maps.googleapis.com/maps/api/js?key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_KEY}`}
-        />
+        ></script>
       </Head>
       <body className="antialiased">
         <Main />
