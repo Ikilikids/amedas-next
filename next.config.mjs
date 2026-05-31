12773c616036e425d0fe322a65c310331e56f812
ikilikid <ponponta1117@gmail.com>
2026-06-01 04:28:34 +0900

    Revert "fix: enable static export to force inclusion of public assets in production"
    This reverts commit 822a4589cf45f2c1c54572866727b4db4c526784.

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,35 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: "export",
-  trailingSlash: true,
+  // output: "export",
+  trailingSlash: false,
   images: {
     unoptimized: true,
+  },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'X-Debug-Deploy',
+            value: 'true',
+          },
+        ],
+      },
+    ];
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/ranking2/:path*',
+        destination: '/ranking2/:path*',
+      },
+      {
+        source: '/infotable/:path*',
+        destination: '/infotable/:path*',
+      },
+    ];
   },
 };
 
